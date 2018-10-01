f915f0e79e8a8625dd9cda76b8ffb66bab2c2401
ed.a.ezekiel <ed.a.ezekiel@gmail.com>
2018-10-01 00:27:03 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,11 +36,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if index.between?(0,8)
-    if !position_taken?(board, index)
-      true
-    end
-  end
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 def turn(board, index, current_player)
