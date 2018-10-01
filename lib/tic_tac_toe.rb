26fe37ebfd7b3c3b802216773390db7fb564bdba
ed.a.ezekiel <ed.a.ezekiel@gmail.com>
2018-10-01 00:23:06 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,8 +27,8 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, player)
-  board[index] = player
+def move(board, index, current_player)
+  board[index] = current_player
 end
 
 def position_taken?(board, index)
