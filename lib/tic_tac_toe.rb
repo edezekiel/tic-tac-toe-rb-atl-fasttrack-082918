2f494a0ec4b9f08bed43b5b5d717a8f6635e7067
ed.a.ezekiel <ed.a.ezekiel@gmail.com>
2018-10-01 00:32:25 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -106,12 +106,5 @@ end
 
 def play(board)
   turn(board)
-  until over?(board)
-    turn(board)
-  end
-  if won?(board)
-    puts "Congratulations #{winner(board)}!"
-  elsif draw?(board)
-    puts "Cat's Game!"
-  end
+
 end
