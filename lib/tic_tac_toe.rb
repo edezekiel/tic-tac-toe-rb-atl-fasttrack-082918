1b3b9007d6f6b5fcdb5711d6067be58de5aa99ad
ed.a.ezekiel <ed.a.ezekiel@gmail.com>
2018-10-01 00:13:07 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,8 +36,11 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
+  if index.between?(0,8)
+    if !position_taken?(board, index)
+      true
+    end
+  end
 
 def turn(board)
   puts "Please enter 1-9:"
